5141b59c540c6acd2d55ac64e0f5958cf68ff73a
Mohanad Ahmed <mhndahmd031@gmail.com>
2025-03-02 01:59:10 +0200

    Add winapi.cpp for DDA line drawing implementation using Windows API

---
+++ src/winapi.cpp
@@ -0,0 +1,96 @@
+/**
+ * @file winapi.cpp
+ * 
+ * @details This code was provided by the computer graphics
+ * professor Prof. Reda Abdelwahab.
+ * It uses the windows api to create a window and draw a line
+ * using DDA algorithm.
+ * This file is separate from the other implementations of other
+ * algorithms which uses OpenGL
+ */
+
+#include <Windows.h>
+
+int Round(double x)
+{
+    return (int)(x + 0.5);
+}
+
+void DrawLineDDA(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c)
+{
+    int dx = x2 - x1, dy = y2 - y1;
+    SetPixel(hdc, x1, y1, c);
+    if (abs(dx) >= abs(dy))
+    {
+        double m = (double)dy / dx;
+        int x = x1;
+        double y = y1;
+        while (x < x2)
+        {
+            x++;
+            y += m;
+            SetPixel(hdc, x, Round(y), c);
+        }
+    }
+    else {
+        double mi = (double)dx / dy;
+        int y = y1;
+        double x = x1;
+        while (y < y2)
+        {
+            y++;
+            x += mi;
+            SetPixel(hdc, Round(x), y, c);
+        }
+    }
+}
+
+LRESULT WndProc(HWND hwnd, UINT m, WPARAM wp, LPARAM lp)
+{
+    HDC hdc;
+    switch (m)
+    {
+    case WM_LBUTTONDOWN:
+        hdc = GetDC(hwnd);
+        DrawLineDDA(hdc, 10, 10, 300, 100, RGB(255, 0, 0));
+        ReleaseDC(hwnd, hdc);
+        break;
+    case WM_LBUTTONUP:
+        hdc = GetDC(hwnd);
+        DrawLineDDA(hdc, 10, 10, 300, 100, RGB(0, 255, 0));
+        ReleaseDC(hwnd, hdc);
+        break;
+    case WM_CLOSE:
+        DestroyWindow(hwnd); break;
+    case WM_DESTROY:
+        PostQuitMessage(0); break;
+    default:return DefWindowProc(hwnd, m, wp, lp);
+    }
+    return 0;
+}
+
+int APIENTRY WinMain(HINSTANCE hi, HINSTANCE pi, LPSTR cmd, int nsh)
+{
+    WNDCLASSW wc; // Use WNDCLASSW
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+    wc.lpszClassName = L"MyClass";
+    wc.lpszMenuName = NULL;
+    wc.lpfnWndProc = WndProc;
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.hInstance = hi;
+    RegisterClassW(&wc); // Use RegisterClassW
+    HWND hwnd = CreateWindowW(L"MyClass", L"Hello World!", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hi, 0); // Use CreateWindowW
+    ShowWindow(hwnd, nsh);
+    UpdateWindow(hwnd);
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
